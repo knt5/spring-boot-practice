6055ac7979348d55ed6f37c57f37238e221d59eb
knt5 <knt5@users.noreply.github.com>
2017-01-06 23:11:19 +0900

    Change insert to use SimpleJdbcInsert to get the assigned key

--- 2.2-SpringJDBC/src/main/java/io/github/knt5/repository/CustomerRepository.java
+++ 2.2-SpringJDBC/src/main/java/io/github/knt5/repository/CustomerRepository.java
@@ -2,12 +2,16 @@ package io.github.knt5.repository;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +22,15 @@ import io.github.knt5.domain.Customer;
 public class CustomerRepository {
 	@Autowired
 	NamedParameterJdbcTemplate jdbcTemplate;
+	
+	SimpleJdbcInsert simpleJdbcInsert;
+	
+	@PostConstruct
+	public void init() {
+		simpleJdbcInsert = new SimpleJdbcInsert((JdbcTemplate) jdbcTemplate.getJdbcOperations())
+				.withTableName("customers")
+				.usingGeneratedKeyColumns("id");
+	}
 	
 	private static final RowMapper<Customer> cutomerRowMapper = (resultSet, rowNum) -> new Customer(
 			resultSet.getInt("id"),
@@ -44,11 +57,11 @@ public class CustomerRepository {
 	public Customer save(Customer customer) {
 		SqlParameterSource param = new BeanPropertySqlParameterSource(customer);
 		if (customer.getId() == null) {
-			jdbcTemplate.update(
-					"INSERT INTO customers(first_name,last_name) values(:firstName, :lastName)",
-					param
-			);
+			// Insert
+			Number key = simpleJdbcInsert.executeAndReturnKey(param);
+			customer.setId(key.intValue());
 		} else {
+			// Update
 			jdbcTemplate.update(
 					"UPDATE customers SET first_name=:firstName,last_name=:lastName WHERE id=:id)",
 					param
