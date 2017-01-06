dc990b25e3eb0ebf82bda221802090f7f82c8de5
knt5 <knt5@users.noreply.github.com>
2017-01-06 20:41:14 +0900

    Update 2.2.1

--- 2.2-SpringJDBC/src/main/java/io/github/knt5/Application.java
+++ 2.2-SpringJDBC/src/main/java/io/github/knt5/Application.java
@@ -1,12 +1,18 @@
 package io.github.knt5;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
+import io.github.knt5.domain.Customer;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -19,11 +25,22 @@ public class Application implements CommandLineRunner {
 	
 	@Override
 	public void run(String... strings) throws Exception {
-		String sql = "SELECT :a + :b";
+		// Create SQL
+		String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
 		SqlParameterSource param = new MapSqlParameterSource()
-				.addValue("a", 100)
-				.addValue("b", 200);
-		Integer result = jdbcTemplate.queryForObject(sql, param, Integer.class);
+				.addValue("id", 3);
+		
+		// Query
+		Customer result = jdbcTemplate.queryForObject(sql, param, new RowMapper<Customer>() {
+			@Override
+			public Customer mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+				return new Customer(
+						resultSet.getInt("id"),
+						resultSet.getString("first_name"),
+						resultSet.getString("last_name")
+				);
+			}
+		});
 		
 		System.out.println("result = " + result);
 	}
---
+++ 2.2-SpringJDBC/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO customers(first_name, last_name) VALUES('Jonathan', 'Joestar');
+INSERT INTO customers(first_name, last_name) VALUES('Joseph', 'Joestar');
+INSERT INTO customers(first_name, last_name) VALUES('Jotaro', 'Kujo');
+INSERT INTO customers(first_name, last_name) VALUES('Josuke', 'Kujo');
