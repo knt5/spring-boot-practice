751e31fb88b79297e2af9741014b6c71bb8a2049
knt5 <knt5@users.noreply.github.com>
2017-01-07 00:21:36 +0900

    Add paging (2.3.4)

--- 2.3-SpringDataJPA/src/main/java/io/github/knt5/Application.java
+++ 2.3-SpringDataJPA/src/main/java/io/github/knt5/Application.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import io.github.knt5.domain.Customer;
 import io.github.knt5.repository.CustomerRepository;
@@ -23,7 +26,23 @@ public class Application implements CommandLineRunner {
 		Customer createdCustomer = customerRepository.save(new Customer(null, "Jolyne", "Kujo"));
 		System.out.println(createdCustomer + " is created.");
 		
-		// Show
+		// Set paging
+		Pageable pageable = new PageRequest(1, 3);
+		Page<Customer> page = customerRepository.findAll(pageable);
+		
+		// Show page data
+		System.out.println("--------------------------------------------");
+		System.out.println("Page size:      " + page.getSize());
+		System.out.println("Page number:    " + page.getNumber());
+		System.out.println("Total pages:    " + page.getTotalPages());
+		System.out.println("Total elements: " + page.getTotalElements());
+		System.out.println("--------------------------------------------");
+		page.getContent().forEach(System.out::println);
+		System.out.println("--------------------------------------------");
+		customerRepository.findAllOrderByName(pageable).forEach(System.out::println);
+		
+		// Show all
+		System.out.println("--------------------------------------------");
 		customerRepository.findAllOrderByName().forEach(System.out::println);
 	}
 }
