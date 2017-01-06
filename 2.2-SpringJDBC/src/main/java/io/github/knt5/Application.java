a57a074c5d12441ba0ddc2b31b048857c3c3ff9d
knt5 <knt5@users.noreply.github.com>
2017-01-06 22:30:42 +0900

    Update CustomerRepository to use DB (2.2.4)

--- 2.2-SpringJDBC/src/main/java/io/github/knt5/Application.java
+++ 2.2-SpringJDBC/src/main/java/io/github/knt5/Application.java
@@ -4,16 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
 import io.github.knt5.domain.Customer;
+import io.github.knt5.repository.CustomerRepository;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 	@Autowired
-	NamedParameterJdbcTemplate jdbcTemplate;
+	CustomerRepository customerRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -21,18 +19,11 @@ public class Application implements CommandLineRunner {
 	
 	@Override
 	public void run(String... strings) throws Exception {
-		// Create SQL
-		String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
-		SqlParameterSource param = new MapSqlParameterSource()
-				.addValue("id", 5);
+		// Add
+		Customer createdCustomer = customerRepository.save(new Customer(null, "Jolyne", "Kujo"));
+		System.out.println(createdCustomer + " is created.");
 		
-		// Query
-		Customer result = jdbcTemplate.queryForObject(sql, param, (resultSet, rowNum) -> new Customer(
-				resultSet.getInt("id"),
-				resultSet.getString("first_name"),
-				resultSet.getString("last_name")
-		));
-		
-		System.out.println("result = " + result);
+		// Show
+		customerRepository.findAll().forEach(System.out::println);
 	}
 }
