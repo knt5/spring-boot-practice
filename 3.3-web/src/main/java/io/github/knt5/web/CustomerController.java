6970327d35fa2921cb5ef6e5917602857f8ee47c
knt5 <knt5@users.noreply.github.com>
2017-01-10 00:06:45 +0900

    Add edit and delete features (3.3.3, 3.3.4)

--- 3.3-web/src/main/java/io/github/knt5/web/CustomerController.java
+++ 3.3-web/src/main/java/io/github/knt5/web/CustomerController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import io.github.knt5.domain.Customer;
 import io.github.knt5.form.CustomerForm;
@@ -50,6 +51,44 @@ public class CustomerController {
 		customerService.create(customer);
 		
 		// Redirect to list view
+		return "redirect:/customers";
+	}
+	
+	@GetMapping(path = "edit", params = "form")
+	public String editForm(@RequestParam Integer id, CustomerForm customerForm) {
+		Customer customer = customerService.findOne(id);
+		BeanUtils.copyProperties(customer, customerForm);
+		return "customers/edit";
+	}
+	
+	@PostMapping(path = "edit")
+	public String edit(
+			@RequestParam Integer id,
+			@Validated CustomerForm customerForm,
+			BindingResult bindingResult
+	) {
+		if (bindingResult.hasErrors()) {
+			return editForm(id, customerForm);
+		}
+		
+		// Update
+		Customer customer = new Customer();
+		BeanUtils.copyProperties(customerForm, customer);
+		customer.setId(id);
+		customerService.update(customer);
+		
+		// Redirect
+		return "redirect:/customers";
+	}
+	
+	@PostMapping(path = "edit", params = "goToTop")
+	public String goToTopFromEdit() {
+		return "redirect:/customers";
+	}
+	
+	@PostMapping(path = "delete")
+	public String delete(@RequestParam Integer id) {
+		customerService.delete(id);
 		return "redirect:/customers";
 	}
 }
